a55a9131488ff06df907814cb92b9ee48a353bf1
xvasil10 <xvasil10@stud.fit.vutbr.cz>
2024-10-06 10:56:40 +0200

    test(commands): Add tests for make_tcp methods

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ CXXFLAGS += -std=c++20
 debug_flags=-Wall -Wenum-compare -Wenum-conversion -Wpedantic -ggdb -O0
 release_flags=-DNDEBUG
 EXE=imapcl
+TESTEXE=imapcl-tests
 LOGIN=xvasil10
 
 all: debug
@@ -15,6 +16,8 @@ zip: $(LOGIN).zip
 
 debug: CXXFLAGS += $(debug_flags)
 debug: $(EXE)
+
+test: $(TESTEXE)
 
 $(EXE): main.o command.o command_factory.o
 	$(CXX) $(CXXFLAGS) $^ -o $@
@@ -26,6 +29,15 @@ command.o: command.cpp
 	$(CXX) $(CXXFLAGS) -c $^ -o $@
 
 command_factory.o: command_factory.cpp
+	$(CXX) $(CXXFLAGS) -c $^ -o $@
+
+$(TESTEXE): test/test_main.o test/make_tcp.o command.o
+	$(CXX) $(CXXFLAGS) -lgtest $^ -o $@
+
+test/test_main.o: test/test_main.cpp
+	$(CXX) $(CXXFLAGS) -c $^ -o $@
+
+test/make_tcp.o: test/make_tcp.cpp
 	$(CXX) $(CXXFLAGS) -c $^ -o $@
 
 clean:
---
+++ test/make_tcp.cpp
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+#include <stdint.h>
+#include <vector>
+#include "../command.hpp"
+
+TEST(CommandMakeTcpTests, Login)
+{
+  std::string expected = std::string("a001 LOGIN username password\r\n");
+  LoginCommand command = LoginCommand("a001", "username", "password");
+
+  std::string result = command.make_tcp();
+
+  EXPECT_EQ(result, expected);
+}
+
+TEST(CommandMakeTcpTests, Select)
+{
+  std::string expected = std::string("a002 SELECT INBOX\r\n");
+  SelectCommand command = SelectCommand("a002", "INBOX");
+
+  std::string result = command.make_tcp();
+
+  EXPECT_EQ(result, expected);
+}
+
+TEST(CommandMakeTcpTests, Search)
+{
+  std::string expected = std::string("a003 SEARCH ALL\r\n");
+  SearchCommand command = SearchCommand("a003", "ALL");
+
+  std::string result = command.make_tcp();
+
+  EXPECT_EQ(result, expected);
+}
+
+TEST(CommandMakeTcpTests, Fetch)
+{
+  std::string expected = std::string("a004 FETCH 1 2 3 5\r\n");
+  std::vector<uint32_t> seq_numbers = {1,2,3,5};
+  FetchCommand command = FetchCommand("a004", seq_numbers);
+
+  std::string result = command.make_tcp();
+
+  EXPECT_EQ(result, expected);
+}
