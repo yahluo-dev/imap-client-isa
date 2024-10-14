8e4d49e1b979acc3e9807479f315b94ef26ea62a
xvasil10 <xvasil10@stud.fit.vutbr.cz>
2024-10-14 16:03:06 +0200

    test(response parsing): Add login parse test

--- test/parse_response.cpp
+++ test/parse_response.cpp
@@ -5,9 +5,14 @@
 
 TEST(ResponseParserParseTests, LoginNoError)
 {
-  // TextResponse expected = TextResponse(RESPONSE_OK, "A001", "LOGIN completed.");
-
   std::string test_data = "A001 OK LOGIN completed.\r\n";
+
+  // response
+  // response-done
+  // response-tagged
+  // tag                 SP resp-cond-state CRLF
+  //                        OK SP resp-text
+  //                               text
 
   ResponseParser parser = ResponseParser(test_data);
 
@@ -23,17 +28,42 @@ TEST(ResponseParserParseTests, SelectResponseNoError)
 
   ResponseParser parser = ResponseParser(test_data[0]);
 
-  EXPECT_TRUE(parser.parse());
+  ASSERT_NO_THROW(parser.parse());
 
   parser = ResponseParser(test_data[1]);
 
-  EXPECT_TRUE(parser.parse());
+  ASSERT_NO_THROW(parser.parse());
 
   parser = ResponseParser(test_data[2]);
 
-  EXPECT_TRUE(parser.parse());
+  ASSERT_NO_THROW(parser.parse());
 
   parser = ResponseParser(test_data[3]);
 
+  ASSERT_NO_THROW(parser.parse());
+}
+
+
+TEST(ResponseParserParseTests, GreetingNoError)
+{
+  std::string test_data = "* OK [CAPABILITY IMAP4rev1 SASL-IR LOGIN-REFERRALS ID ENABLE IDLE LITERAL+ STARTTLS AUTH=PLAIN AUTH=LOGIN] Dovecot ready.\r\n";
+
+  ResponseParser parser = ResponseParser(test_data);
+
   EXPECT_TRUE(parser.parse());
+}
+
+TEST(ResponseParserParseTests, LoginParseCorrect)
+{
+  std::unique_ptr<Response> expected = std::make_unique<StatusResponse>(ResponseType::OK, "A001", "OK LOGIN completed.");
+  std::string test_data = "A001 OK LOGIN completed.\r\n";
+
+  ResponseParser parser = ResponseParser(test_data);
+
+  std::shared_ptr<Response> result;
+
+  ASSERT_NO_THROW((result = parser.parse()));
+
+  ASSERT_EQ(expected->get_tag(), result->get_tag());
+  ASSERT_EQ(expected->get_type(), result->get_type());
 }
